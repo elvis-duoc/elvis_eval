ea97d32cbae3a3977461585c365744247ff33621
elvis-duoc <elv.andrade@duocuc.cl>
2021-07-14 18:05:49 -0400

    admin.site.register(Contacto) -- AGREGADO

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Login
+from .models import Login, Contacto
 # Register your models here.
 
 admin.site.register(Login)
+admin.site.register(Contacto)
